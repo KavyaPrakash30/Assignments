ba614e45bd911214b792c6105914cb0a5f98a4e6
KavyaPrakash30 <69236533+KavyaPrakash30@users.noreply.github.com>
2022-03-13 20:55:29 +0530

    Add files via upload

---
+++ src/Assignment1/CopyMethod.java
@@ -0,0 +1,23 @@
+package Assignment1;
+
+
+
+	import java.util.Arrays;
+	
+	public class CopyMethod {
+		public static void main(String args[])
+		{
+	int[] org = new int[] {1, 2, 3};
+	        System.out.println("Original Array");
+	        for (int i= 0; i< org.length; i++)
+	        	System.out.print(org[i] +"");
+	        
+	        int[] copy = Arrays.copyOf(org, 5);
+	        copy[3] = 11;
+	        copy[4] = 55;
+	        
+	        System.out.println("\nNew array copy after modification:");
+	        for (int i=0; i< copy.length; i++)
+	        	System.out.print(copy[i] + " ");
+		}
+	}
---
+++ src/Assignment1/GradeAverage.java
@@ -0,0 +1,32 @@
+package Assignment1;
+import java.util.Scanner;
+public class GradeAverage {
+
+	
+	
+public static void main(String[] args)
+{ int avg=0, grade = 0;
+  try (Scanner abc = new Scanner(System.in)) {
+	  System.out.println("enter no of student");
+	  int n=abc.nextInt();
+	  for(int i=1; i<=n; i++)
+	  {
+		  System.out.println("Enter the grade of student" +i);
+		  
+		      grade = abc.nextInt();
+		      if(0<=grade && grade<=100) {
+		    	  avg+=grade;
+		    	  
+		      }
+		      else
+		      {
+		    	  System.out.println("Invalid grade, try again");
+		    	  i=i-1;
+		      }
+	  }
+	  System.out.println("The average is "+ avg/n);
+  }
+}
+}
+
+
